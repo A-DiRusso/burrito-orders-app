bb1d03a23e9f10bcbd9e6194ee9e6e7fa0813bdb
Anthony DiRusso <ossurid@gmail.com>
2019-03-29 11:07:55 -0400

    adds models/conn.js

---
+++ models/conn.js
@@ -0,0 +1,18 @@
+//first require 'pg-promise'
+//call it immidetly which gives you a configured database connector
+//define the connection options
+const pgp = require('pg-promise')({
+    query: e => {
+        console.log('QUERY: ', e.query);
+    }
+});
+
+const options = {
+    host: 'localhost',
+    database: 'coffee-orders-app'
+};
+
+
+//make a connection to the database specified by the options object
+const db = pgp(options);
+module.exports = db;

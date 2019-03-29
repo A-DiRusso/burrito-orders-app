59fb578a45e0a023e4b06e1ad95fe486b30eb884
Anthony DiRusso <ossurid@gmail.com>
2019-03-29 10:52:04 -0400

    creates skeleton for burrito-orders-app

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table burrito-orders (
+    id serial primary key,
+    name varchar(200),
+    order varchar(200),
+    heat varchar(200)
+);
---
+++ seed.sql
@@ -0,0 +1,8 @@
+insert into burrito-orders
+        (name, order, heat)
+values
+    ('Beth', 'Vegan', 'lava'),
+    ('Bob', 'Vegitarian', 'mild'),
+    ('Betty', 'Bacon', 'hot'),
+    ('Bill', 'Carne', 'none')
+;
